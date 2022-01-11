1aebda9f3492818eaca1d429fcb61ab0040d6d8b
matti-aleve <89364342+matti-aleve@users.noreply.github.com>
2022-01-11 18:28:52 -0500

    automated

--- Scripts/Covid_PositivityPlot.py
+++ Scripts/Covid_PositivityPlot.py
@@ -41,21 +41,17 @@ strToday = datetime.today().strftime('%Y-%m-%d')
 filename = "/Users/matti/Desktop/Covid/covidtesting_"+strToday+".csv"
 
 
-hostInfo = os.uname()
-
-
-
 
 reported_date = 0
 pctPos = 10
+tests = 9
 
 
 	
 count = 0
-
 fullline = ""
-
 PctPosLastDay = {}
+TestsLastDay = {}
 
 
 
@@ -74,6 +70,10 @@ with open(filename, newline='') as csvfile:
 				PctPosLastDay[line[reported_date]] = float(line[pctPos])
 			except:
 				PctPosLastDay[line[reported_date]] = 0
+			try:
+				TestsLastDay[line[reported_date]] = float(line[tests])
+			except:
+				TestsLastDay[line[reported_date]] = 0
 
 ## PctPos
 
@@ -113,3 +113,41 @@ plt.ylabel('%')
 plt.savefig('/Users/matti/GitHub/casePlots/pos.png')
 plt.clf()
 
+## Tests
+
+chartPoint = []
+chartX = []
+for i in sorted(TestsLastDay):
+	refDateStr = "2021-12-01"
+	if(datetime.strptime(i,'%Y-%m-%d') > datetime.strptime(refDateStr,'%Y-%m-%d')):
+		chartX.append(i[-5:])
+		chartPoint.append(TestsLastDay[i])
+		print(i)
+		print(TestsLastDay[i])
+		#chartX.append(i)
+		
+
+xdates = [dt.strptime(dstr,'%m-%d') for dstr in chartX]
+
+num_series = pd.Series(chartPoint)
+windows = num_series.rolling(7)
+mov_avg = windows.mean()
+
+mov_avg_list = mov_avg.tolist()
+
+daysToPlot = -15
+
+
+#plt.plot(chartX[daysToPlot:],chartPoint[daysToPlot:],'r.')
+plt.plot(chartX,chartPoint,'r.')
+plt.title('Tests Completed -' + strToday)
+
+plt.setp(plt.gca().xaxis.get_majorticklabels(),rotation=60)
+plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=3))
+plt.gca().xaxis.set_minor_locator(mdates.DayLocator())
+plt.xlabel('Date')
+plt.ylabel('%')
+
+
+plt.savefig('/Users/matti/GitHub/casePlots/tests.png')
+plt.clf()
